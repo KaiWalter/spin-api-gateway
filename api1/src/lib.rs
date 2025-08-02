62abd2ba72e93f703e6f94f9b1ae859673ad4832
Kai Walter <kai.walter@outlook.de>
2025-08-02 11:24:56 +0200

    callback to host

--- api1/src/lib.rs
+++ api1/src/lib.rs
@@ -9,13 +9,25 @@ struct Api;
 
 impl Guest for Api {
     fn handle_api_request(request: ApiRequest) -> ApiResponse {
+        // Call the host callback function
+        let host_request = host::ApiRequest {
+            method: request.method.clone(),
+            path: format!("{}-from-api1", request.path),
+            headers: request.headers.clone(),
+            body: request.body.clone(),
+        };
+        
+        let host_response = host::host_api_request(&host_request);
+        
         let debug_info = format!(
-            "[API1 Debug]\nMethod: {}\nPath: {}\nHeaders: {:#?}\nBody: {}",
+            "[API1 Debug]\nOriginal Request - Method: {}, Path: {}\nHost Response - Status: {}, Body: {}\nOriginal Headers: {:#?}",
             request.method,
             request.path,
-            request.headers,
-            request.body.as_ref().map(|b| String::from_utf8_lossy(b)).unwrap_or_else(|| "<none>".into())
+            host_response.status,
+            host_response.body.as_ref().map(|b| String::from_utf8_lossy(b)).unwrap_or_else(|| "<none>".into()),
+            request.headers
         );
+        
         ApiResponse {
             status: 200,
             headers: vec![("content-type".to_string(), "text/plain".to_string())],
